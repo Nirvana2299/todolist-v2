f196fc03acf8bf96978e5765ffaad878c83e3378
Nirvana <shoaibarms@gmail.com>
2023-02-16 02:26:27 +0530

    modified:   app.js modified:   package-lock.json modified:   package.json modified:   views/list.ejs

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
 
 app.set('view engine', 'ejs');
-mongoose.set('strictQuery', true);
+mongoose.set('strictQuery', true); //to get rid of mongoose deprication error
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -28,8 +29,6 @@ const listSchema = { name: String, items: [itemsSchema] };
 const List = mongoose.model("list", listSchema);
 
 
-
-
 app.get("/", (req, res) => {
 
   Item.find({}, (err, items) => {
@@ -47,26 +46,48 @@ app.get("/", (req, res) => {
 //app.post for adding new list items to the database
 app.post("/", (req, res) => {
   const itemName = req.body.newItem;
+  const listName = req.body.list;
   const newName = new Item({ name: itemName }); //simpler way add the value or u can do it using model.insertMany
-  newName.save();
+
+  if (listName === "Today") {
+    newName.save();
+    res.redirect("/");
+  } else {
+    List.findOne({ name: listName }, (err, foundList) => {
+      if (err) {
+        console.log(err)
+      } else {
+        foundList.items.push(newName);
+        foundList.save();
+        res.redirect(`/${listName}`);
+      };
+      
+    }); 
+  }
   // below commented out code does the same thing.
   // Item.insertMany({name: itemName}, (err, doc) => {if (err) {console.log(err)} else {console.log(doc);}});
-  res.redirect("/");
-
 });
 
 
 //app.post for deleting the cross list for the database
 app.post("/delete", (req, res) => {
   const toDelete = req.body.checkbox;
+  const listName = req.body.listName;
   console.log(toDelete);
-  Item.findByIdAndRemove({ _id: toDelete }, (err) => { if (err) { console.log(err) } else { console.log("successfully deleted from the list") } });
-  res.redirect("/")
-
+  if (listName === "Today") {
+    Item.findByIdAndRemove({ _id: toDelete }, (err) => { if (err) { console.log(err) } else { console.log("successfully deleted an Item from the Today list") } });
+    res.redirect("/")
+  } else {
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: toDelete }}}, (err, results) => {
+      if (!err) {
+        res.redirect(`/${listName}`)
+      }
+    });
+  }
 });
 
 app.get("/:listName", (req, res) => {
-  const listName = req.params.listName;
+  const listName = _.capitalize(req.params.listName);
 
   List.findOne({ name: listName }, (err, results) => {
     if (!err) if (!results) {
@@ -74,8 +95,10 @@ app.get("/:listName", (req, res) => {
         name: listName,
         items: defaultItems
       });
-      dynamicList.save();
-      res.redirect(`/${listName}`)
+      dynamicList.save((err, results) => {
+        res.redirect(`/${listName}`);
+      });
+      
     } else {
       res.render("list", { listTitle: results.name, newListItems: results.items })
     }
